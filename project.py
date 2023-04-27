d6f93092598754793c3cfb71a4ff457a1b950598
Rashel Rojas <rdr2139@columbia.edu>
2023-04-27 16:27:34 -0400

    finished title heuristic

--- project.py
+++ project.py
@@ -10,25 +10,67 @@ from pytesseract import Output
 import re
 
 
+def clean_title(max_val):
+    title_words = max_val.split()
+    cleaned_title = []
+    for word in title_words:
+        hasNums = False
+        for ch in word:
+            if ch.isnumeric():
+                hasNums = True
+                break
+        if not hasNums:
+            cleaned_title.append(word)
+    new_title = ' '.join(cleaned_title)
+    return new_title
+
+# Source: https://stackoverflow.com/questions/20831612/getting-the-bounding-box-of-the-recognized-words-using-python-tesseract/54059166#54059166
+def get_sentence_info(cropped): 
+    d = pytesseract.image_to_data(cropped, output_type=Output.DICT)
+    n_boxes = len(d['level'])
+    word_heights = {}
+    words = {}
+    for i in range(n_boxes):
+        if d['text'][i] != '':
+            word_heights[i] = d['height'][i]
+            words[i] = d['text'][i]
+        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+        cv2.rectangle(cropped, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    avg_word_height = sum (word_heights.values()) / len(word_heights)
+    sentence = ' '.join(words.values())
+    return (avg_word_height, sentence)
+
+
 def main():
-    image = cv2.imread('noback.jpg')
+    image = cv2.imread('images/noback2.jpg')
 
-    #///// Preprocessing Image /////
-
+    ### Preprocessing Image ###
     # Source: https://www.geeksforgeeks.org/text-detection-and-extraction-using-opencv-and-ocr/
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | 
                                             cv2.THRESH_BINARY_INV)
-    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 50))
+    #thresh = cv2.bitwise_not(thresh) ## new
+    ksize = 50
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (ksize, ksize))
     dilation = cv2.dilate(thresh, rect_kernel, iterations = 1)    
     contours, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
                                              cv2.CHAIN_APPROX_NONE)
 
     im2 = image.copy()
+    
+    # Remove dark wall from image
+    '''x, y, w, h = cv2.boundingRect(contours[0])
+    rect = cv2.rectangle(thresh, (x, y), (x + w, y + h), (0, 255, 0), 2)        
+    cropped = thresh[y:y + h, x:x + w] 
+    cv2.imshow('img', thresh)
+    cv2.waitKey(0)'''
+
     file = open("recognized.txt", "w+")
     file.write("")
     file.close()
 
+    possible_titles = {}
+    j = 0
     print(len(contours))
     for cnt in contours:
         # Source: https://stackoverflow.com/questions/24385714/detect-text-region-in-image-using-opencv 
@@ -37,34 +79,31 @@ def main():
         cropped = thresh[y:y + h, x:x + w] # was im2
         file = open("recognized.txt", "a")
         text = pytesseract.image_to_string(cropped)
+        #cv2.imshow('img', cropped)
+        #cv2.waitKey(0)
 
-        ### get title
-        '''d = pytesseract.image_to_data(cropped, output_type=Output.DICT)
-        n_boxes = len(d['level'])
-        for i in range(n_boxes):
-            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-            cv2.rectangle(cropped, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Get title    
+        possible_titles[j] = get_sentence_info(cropped) 
+        j += 1
 
-        cv2.imshow('img', cropped)
-        cv2.waitKey(0)'''
-
-
-
-
-        newtext = ""
+        '''newtext = ""
         for i in range(1, len(text)):
             if text[i] == '\n' and text[i - 1] == '\n':
                 newtext += " "
             else:
                 newtext += text[i]
         regions = re.split("\n{2}", text)
-        print(regions)
-        
+        print(regions)'''        
         # then split string by whitespace, get rid of duplicates?
         # actually: just look for date and time, once found, stop searching
         
         file.write(text)
         file.write("\n")        
-        file.close()            
+        file.close()   
+    
+    # title is sentence with max height
+    max_val = max(possible_titles.values(), key=lambda sub: sub[0])[1]
+    new_title = clean_title(max_val)
+    print(new_title)    
 
 main()
